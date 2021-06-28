dfda69a9f95d7202b0960092b108ea099db5be5a
Renan Pereira <renan.pereira@xpi.com.br>
2021-06-28 14:57:56 -0300

    acertar nome

--- src/Renan.GlassLewis.Domain/Company/CompanyConstants.cs
+++ src/Renan.GlassLewis.Domain/Company/CompanyConstants.cs
@@ -4,7 +4,7 @@
     {
         public const string CompanyWithSameIsinAlreadyExists = "Company with the same Isin already exists";
         public const string CompanyIsinMustHaveValue = "Value must have a Isin";
-        public const string CompanyIsinMustBeGreaterThenTwoChars = "Company Value must be greater then 2 caracteres";
+        public const string CompanyIsinMustBeGreaterThenTwoChars = "Company Isin must be greater then 2 caracteres";
         public const string CompanyIsinMustStartWithTwoLetter = "Isin must start with 2 letters";
         public const string CompanyIdMustExist = "Company Id Must Exist";
         public const string NameIsRequired = "Name is Required";
